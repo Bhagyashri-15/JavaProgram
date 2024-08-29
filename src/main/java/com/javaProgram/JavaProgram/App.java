0977bdd608f2bf58b11c5d3c917ca422a2c1534e
Lenovo <bhagyashri.pathare12345@gmail.com>
2024-08-29 13:52:33 +0530

    commit msg1

---
+++ src/main/java/com/javaProgram/JavaProgram/App.java
@@ -0,0 +1,13 @@
+package com.javaProgram.JavaProgram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
