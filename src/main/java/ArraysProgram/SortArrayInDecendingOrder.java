4152bf6f9c973d631ad9eb3e8ef3651940ecdb8e
Lenovo <bhagyashri.pathare12345@gmail.com>
2024-08-29 14:55:12 +0530

    commit code msg2

---
+++ src/main/java/ArraysProgram/SortArrayInDecendingOrder.java
@@ -0,0 +1,27 @@
+package ArraysProgram;
+
+public class SortArrayInDecendingOrder {
+	
+	public static void main(String[] args) {
+		
+		int [] a = {15,5,22,81,7};
+		
+		int temp;
+		
+		for(int i = 0;i<=a.length-1;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+			if(a[i]<a[j])
+			{
+				temp = a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+			}
+			System.out.print(a[i]+" ");
+		}
+	}
+
+
+}
